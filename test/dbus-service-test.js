d6b7d9775f6bcee871a54daa1a45a38cbcda75f4
Todd Wolfson <todd@twolfson.com>
2016-10-19 04:53:54 -0700

    Taking notes on helpful debug items

--- test/dbus-service-test.js
+++ test/dbus-service-test.js
@@ -2,6 +2,15 @@
 var assert = require('assert');
 var dbusService = require('../');
 
+// jscs:disable maximumLineLength
+// Test commands:
+//   Monitor traffic:
+//     dbus-monitor
+//   List all services:
+//     https://dbus.freedesktop.org/doc/dbus-specification.html#bus-messages-list-names
+//     dbus-send --print-reply --type=method_call --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.ListNames
+// jscs:enable maximumLineLength
+
 // Start our tests
 describe('dbus-service', function () {
   it('returns awesome', function () {
